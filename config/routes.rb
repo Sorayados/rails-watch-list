3ab11bcf899d2bea0012e5ceec501928b8303c9f
Soraya Dos Santos <dossantos.soraya@gmail.com>
2024-04-26 10:48:04 +0200

    Watch List

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root to: "lists#index"
   resources :lists do
-    resources :bookmarks, only: %i[index show new create]
+    resources :bookmarks, only: %i[show new create]
   end
   resources :bookmarks, only: :destroy
 
